cdb6d9f64b85fd07d6aa201140045dec78b212b6
Oleh <olegbacara@gmail.com>
2022-05-04 20:03:46 +0300

    index upgrade

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 
 <?php include("includes/header.php"); ?>
 <p><label  style="font-family:fantasy; font-size: 100px; text-align:right; color: #ffa64d; text-shadow: orange 0 0 4px;"  for="user_pass">ITwork PORTAL</label></p>
+<form action="login.php">
+<p class="submit"><input class="button" name="login"type= "submit" value="Log In"></p>
+</form>
+<form action="login.php">
+<p class="submit"><input class="button" name="login"type= "submit" value="Administrator"></p>
+</form>
 
-<p class="regtext">Not registered yet?<a href= "login.php">Login</a>!</p>
-<p class="regtext">Admin Panel<a href= "admin.php">Admin</a>!</p>
+
 <?php include("includes/footer.php"); ?>
