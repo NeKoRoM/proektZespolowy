d2abf0230e75c3dd223a44278a67be15cfaf41b2
Oleh <olegbacara@gmail.com>
2022-05-04 19:25:33 +0300

    Admin panel

--- index.php
+++ index.php
@@ -1,1 +1,7 @@
-<p class="regtext">Not registered yet?<a href= "login.php">Login</a>!</p>s
+
+<?php include("includes/header.php"); ?>
+<p><label  style="font-family:fantasy; font-size: 100px; text-align:right; color: #ffa64d; text-shadow: orange 0 0 4px;"  for="user_pass">ITwork PORTAL</label></p>
+
+<p class="regtext">Not registered yet?<a href= "login.php">Login</a>!</p>
+<p class="regtext">Admin Panel<a href= "admin.php">Admin</a>!</p>
+<?php include("includes/footer.php"); ?>
